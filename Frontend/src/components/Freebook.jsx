e6f819ba72c59013bb6bac0a468bf45e754100e0
Priyanka7081 <priyankau7081@gmail.com>
2026-05-15 18:00:19 +0530

    Update API endpoint for fetching free books

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://book-store-backend-22nm.onrender.com");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
